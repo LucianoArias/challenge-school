e89f59f4a9230c674b25865f3b34c74945a731e8
LucianoArias <lucianoarias1997@gmail.com>
2023-01-04 11:05:23 -0300

    url

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:
-    'mysql://root:OauxdFHkeXFvwisy5v5q@containers-us-west-30.railway.app:7611/railway/api',
+  baseURL: 'https://challenge-school.vercel.app/api',
   // baseURL: 'http://localhost:5000/api', // localhost
 });
 
