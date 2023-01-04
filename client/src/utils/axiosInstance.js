2a5bcac4401595d64cf2b2f0d23db5ee427c64c2
LucianoArias <lucianoarias1997@gmail.com>
2023-01-04 08:52:18 -0300

    url

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL:
-    'mysql://root:OauxdFHkeXFvwisy5v5q@containers-us-west-30.railway.app:7611/railway',
+    'mysql://root:OauxdFHkeXFvwisy5v5q@containers-us-west-30.railway.app:7611/railway/api',
   // baseURL: 'http://localhost:5000/api', // localhost
 });
 
