6e876058bff17be720e0e15cf7b8e9e3b16ad00b
LucianoArias <lucianoarias1997@gmail.com>
2023-01-05 17:00:25 -0300

    deploy

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,18 +1,8 @@
 import axios from 'axios';
 
-const url = require('url');
-
-// let DATABASE_URL = url.parse(
-//   'mysql://root:OauxdFHkeXFvwisy5v5q@containers-us-west-30.railway.app:7611/railway/api'
-// );
-// DATABASE_URL.protocol = 'https:';
-// console.log(DATABASE_URL.protocol);
-
 const axiosInstance = axios.create({
-  REACT_APP_BASE_URL: 'https://challenge-school-production.up.railway.app/',
-
+  baseURL: 'https://challenge-school-production.up.railway.app/api',
   // baseURL: 'http://localhost:5000/api', // localhost
 });
-// console.log(DATABASE_URL);
 
 export default axiosInstance;
