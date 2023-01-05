05992d2c20687d71584ad83ca2de899e640f8d3a
LucianoArias <lucianoarias1997@gmail.com>
2023-01-05 03:45:33 -0300

    url protocol

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
 
+const url = require('url');
+
+let u = url.parse(
+  'mysql://root:OauxdFHkeXFvwisy5v5q@containers-us-west-30.railway.app:7611/railway/api'
+);
+u.protocol = 'http:';
+console.log(u.protocol);
+
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  u: 'mysql://root:OauxdFHkeXFvwisy5v5q@containers-us-west-30.railway.app:7611/railway/api',
+
   // baseURL: 'http://localhost:5000/api', // localhost
 });
+console.log(u);
 
 export default axiosInstance;
