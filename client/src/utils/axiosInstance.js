2daea3debe09cafb3f32f863710989b842ce54eb
Luciano <lucianoarias1997@gmail.com>
2024-02-19 21:16:17 -0300

    update db

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://challenge-school-production.up.railway.app/api',
+  baseURL: 'https://challenge-school.vercel.app/api',
   // baseURL: 'http://localhost:5000/api', // localhost
 });
 
